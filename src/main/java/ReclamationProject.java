0bbdefea670970448b854ff287de545d70205aa2
abhitekula <abhitekula@gmail.com>
2017-09-26 10:27:28 -0500

    Fixed all checkstyle errors

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,26 +1,33 @@
-/*
- * The following code needs a lot of TLC. So give it some!
- *
- * 1. Fix all checkstyle errors
- * 2. Determine what it does (it's going to be tough before you finish #1)
- * 3. Improve the name of the methods and variables
- * 4. Add comments and Javadoc comments where needed
- * 5. Remove unnecessary comments as appropriate
+/**
+ * The following code needs a lot of TLC. So give it some! 1. Fix all checkstyle errors 2. Determine
+ * what it does (it's going to be tough before you finish #1) 3. Improve the name of the methods and
+ * variables 4. Add comments and Javadoc comments where needed 5. Remove unnecessary comments as
+ * appropriate Finds the longest common substring
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+public class ReclamationProject {
+    /**
+     * @param a first input
+     * @param b second input
+     * @return returns longest common subtsring
+     */
+    static String doit(final String a, final String b) {
+        String a1 = a, b1 = b;
+        if (a.length() > b.length()) {
+            final String c = a;
+            a1 = b;
+            b1 = c;
+        }
+        String r = "";
+        for (int i = 0; i < a1.length(); i++) {
+            for (int j = a1.length() - i; j > 0; j--) {
+                for (int k = 0; k < b1.length() - j; k++) {
+                    if (a1.regionMatches(i, b1, k, j) && j > r.length()) {
+                        a1.substring(i, i + j);
+                    }
+                }
+            }
+        }
+        return r;
+    }
 }
